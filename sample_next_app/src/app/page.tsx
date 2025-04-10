c295e1001dc31a876e29733ed1b3f47db5cea381
sylee <th42500@naver.com>
2025-04-11 00:00:10 +0900

    Tailwind CSS 적용 실습 진행중

--- sample_next_app/src/app/page.tsx
+++ sample_next_app/src/app/page.tsx
@@ -1,10 +1,29 @@
-import Image from "next/image";
+'use client';
+
+import { useState } from "react";
 
 export default function Home() {
+  var [input, setInput] = useState("");
+  var [message, setMessage] = useState("your name:");
+
+  const doChange = (e) => {
+    setInput(e.target.value);
+  }
+
+  const doClick = () => {
+    setMessage("Hello, " + input + "!!");
+    setInput("");
+  }
+
   return (
     <main>
-      <h1 className="text-2xl m-5">Next.js sample.</h1>
-      <p className="text-lg m-5">This is sample application.</p>
+      <h1 className="text-2xl m-5 text-red-500">Next.js sample.</h1>
+      <p className="text-lg m-5">{message}</p>
+      <div className="m-5">
+        <input type="text" onChange={doChange} value={input} className="p-1 border-solid border-2 border-gray-400" />
+        <button onClick={doClick} className="px-7 py-2 mx-2 bg-blue-800 text-white rounded-lg"> Click
+        </button>
+      </div>
     </main>
   );
-}
+}  
