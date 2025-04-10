7c2b9d427581625cd7efb1cac9099f777f943ffa
sylee <th42500@naver.com>
2025-04-10 23:07:54 +0900

    first commit

---
+++ sample_next_app/src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-2xl m-5">Next.js sample.</h1>
+      <p className="text-lg m-5">This is sample application.</p>
+    </main>
+  );
+}
