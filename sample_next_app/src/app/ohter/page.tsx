a42dfc5d337055331f872fd06f81505b21967a03
sylee <th42500@naver.com>
2025-04-12 21:12:54 +0900

    Image 컴포넌트를 이용한 정적 콘텐츠 표시

--- sample_next_app/src/app/ohter/page.tsx
+++ sample_next_app/src/app/ohter/page.tsx
@@ -1,10 +1,14 @@
 import Link from 'next/link';
+import Image from 'next/image';
 
 export default function Home() {
     return (
         <main>
             <h1 className="title">Other page</h1>
             <p className="msg">이건 다른 페이지입니다.</p>
+            <div>
+                <Image src="/quakka.jpg" width={200} height={200} alt='quakka.jpg' />
+            </div>
             <div>
                 <Link href="/">go back!!</Link>
             </div>
