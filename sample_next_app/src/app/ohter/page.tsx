8729cade42393268124616f6c7e27b5eff72b34e
sylee <th42500@naver.com>
2025-04-12 23:04:06 +0900

    Styled JSX 이용하기

--- sample_next_app/src/app/ohter/page.tsx
+++ sample_next_app/src/app/ohter/page.tsx
@@ -1,13 +1,24 @@
+'use client';
+
 // import './style.css';
-import styles from './style.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
+import styles from './style.module.css';
+import JSXStyle from 'styled-jsx/style';
 
 export default function Other() {
     return (
         <main>
+            <JSXStyle>
+                {`p.jsx-msg {
+                margin: 10px;
+                text-align: center;
+                color: red;
+                font-weight: bold;
+                }`}
+            </JSXStyle>
             <h1 className={styles.title}>Other page</h1>
-            <p className={styles.msg}>이건 다른 페이지입니다.</p>
+            <p className="jsx-msg">이건 다른 페이지입니다.</p>
             <div>
                 <Image src="/quakka.jpg" width={200} height={200} alt='quakka.jpg' />
             </div>
