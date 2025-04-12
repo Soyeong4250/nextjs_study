29be6df1aef79cee42f4c11f7133eb03a93622f0
sylee <th42500@naver.com>
2025-04-12 22:42:12 +0900

    CSS 모듈을 이용한 동적 클래스 적용

--- sample_next_app/src/app/ohter/page.tsx
+++ sample_next_app/src/app/ohter/page.tsx
@@ -1,12 +1,13 @@
-import './style.css';
+// import './style.css';
+import styles from './style.module.css';
 import Link from 'next/link';
 import Image from 'next/image';
 
 export default function Home() {
     return (
         <main>
-            <h1 className="title">Other page</h1>
-            <p className="msg">이건 다른 페이지입니다.</p>
+            <h1 className={styles.title}>Other page</h1>
+            <p className={styles.msg}>이건 다른 페이지입니다.</p>
             <div>
                 <Image src="/quakka.jpg" width={200} height={200} alt='quakka.jpg' />
             </div>
