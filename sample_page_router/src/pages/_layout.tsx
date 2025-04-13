ac5d4642478cbd1697324f839b769b7604e92b3a
sylee <th42500@naver.com>
2025-04-13 15:46:08 +0900

    정적 속성 레이아웃에 전달하기

--- sample_page_router/src/pages/_layout.tsx
+++ sample_page_router/src/pages/_layout.tsx
@@ -1,7 +1,7 @@
-export default function Layout({children}: {children: any}) {
+export default function Layout({children, data}: {children: React.ReactNode, data:{title:string}}) {
     return (
         <>
-            <h1 className="header">Next Application</h1>
+            <h1 className="header">{data?.title}</h1>
             <main>{children}</main>
             <hr className="footer" />
             <p className="footer">copyright 2023 SYODA-Tuyano.</p>
