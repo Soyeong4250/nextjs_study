03b4897079e63746698365389f1dc52f7a85b3b8
sylee <th42500@naver.com>
2025-04-13 14:26:59 +0900

    페이지 라우터 Link 태그 적용

--- sample_page_router/src/pages/index.tsx
+++ sample_page_router/src/pages/index.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -16,6 +17,7 @@ export default function Home() {
     <main>
       <h1>SSG application.</h1>
       <p>This is sample page.</p>
+      <div><Link href="/other">Go "Other".</Link></div>
     </main>
   );
 }
