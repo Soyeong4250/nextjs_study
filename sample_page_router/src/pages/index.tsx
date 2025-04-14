b747103d986fd2c5e5e24881d5f578fb5e2e4f5c
sylee <th42500@naver.com>
2025-04-14 17:32:40 +0900

    getStaticProps 함수를 이용한 정적 속성 이용하기

--- sample_page_router/src/pages/index.tsx
+++ sample_page_router/src/pages/index.tsx
@@ -1,23 +1,23 @@
 import { Inter } from "next/font/google";
 import Link from "next/link";
-import { GetStaticProps } from 'next';
+// import { GetStaticProps } from 'next';
 
-export const getStaticProps = ((context) => {
-  const data = {
-    title: "Index page",
-    msg: "시작페이지입니다."
-  }
-  return {props: {data}}
-})
+// export const getStaticProps = ((context) => {
+//   const data = {
+//     title: "Index page",
+//     msg: "시작페이지입니다."
+//   }
+//   return {props: {data}}
+// })
 
 
 const inter = Inter({subsets: ['latin']});
 
-export default function Home({data}: {data:{msg:string}}) {
+export default function Home() {
   return (
     <main>
-      <p>{data.msg}</p>
-      <div><Link href="/other">Go "Other".</Link></div>
+      <h1 className="header">Static page</h1>
+      <p>정적 페이지입니다. 빌드 시에 렌더링됩니다.</p>
     </main>
   );
 }
